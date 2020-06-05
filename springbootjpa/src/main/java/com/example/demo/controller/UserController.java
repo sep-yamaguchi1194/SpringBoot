569d632d5fc4383f53cfbff18b96e7655ff666fc
81801 <81801@192.168.1.4>
2020-06-05 15:58:55 +0900

    Spring Boot - Hello World

--- springbootjpa/src/main/java/com/example/demo/controller/UserController.java
+++ springbootjpa/src/main/java/com/example/demo/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.dto.UserRequest;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 
@@ -35,5 +41,38 @@ public class UserController {
         List<User> userlist = userService.searchAll();
         model.addAttribute("userlist", userlist);
         return "user/list";
+    }
+
+    /**
+     * ユーザー新規登録画面の表示
+     * @param model Model
+     * @return ユーザー新規登録のHTML
+     */
+    @RequestMapping(value = "/user/add")
+    public String displayAdd(Model model) {
+        model.addAttribute("userRequest", new UserRequest());
+        return "user/add";
+    }
+
+    @RequestMapping(value = "/user/create", method = RequestMethod.POST)
+    public String create(@Validated @ModelAttribute UserRequest userRequest, BindingResult result, Model model) {
+        /**
+         * エラーチェック
+         */
+        if(result.hasErrors()) {
+            List<String> errorList = new ArrayList<String>();
+            for(ObjectError error : result.getAllErrors()) {
+                errorList.add(error.getDefaultMessage());
+            }
+
+            model.addAttribute("validationError", errorList);
+            return "user/add";
+        }
+
+        /*
+         * ユーザー情報の登録
+         */
+        userService.create(userRequest);
+        return "redirect:/user/list";
     }
 }
--- springbootjpa/src/main/java/com/example/demo/service/UserService.java
+++ springbootjpa/src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -7,6 +8,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.UserRequest;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 /**
@@ -15,6 +17,7 @@ import com.example.demo.repository.UserRepository;
 @Service
 @Transactional(rollbackOn = Exception.class)
 public class UserService {
+
     /**
      * User Information Repository
      */
@@ -22,9 +25,26 @@ public class UserService {
     UserRepository userRepository;
 
     /**
-     * @return
+     * User Information All Search
+     * @return ユーザー情報検索結果
      */
     public List<User> searchAll() {
         return userRepository.findAll();
+    }
+
+    /**
+     * User Information Add
+     * @param user ユーザー情報
+     */
+    public void create(UserRequest userRequest) {
+        Date now = new Date();
+
+        User user = new User();
+        user.setName(userRequest.getName());
+        user.setAddress(userRequest.getAddress());
+        user.setPhone(userRequest.getPhone());
+        user.setCreateDate(now);
+        user.setUpdateDate(now);
+        userRepository.save(user);
     }
 }
